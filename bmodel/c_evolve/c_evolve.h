55106f4a2c194ad139c573feecceb5b85395d8d3
ruhugu <rubenhugu@gmail.com>
2017-11-13 10:49:07 +0100

    Fixed package folder name bmodel to bdmodel.

--- bmodel/c_evolve/c_evolve.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "dranxor2/dranxor2C.h"
-#include <stdlib.h>
-
-void c_evolve(long int* in_heights, long int* out_heights, int length, 
-              long int nsteps);
-long int max_three(long int a, long int b, long int c);
